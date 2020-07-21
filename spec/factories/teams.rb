a9deac5cd942a23ea05b5ce8e4f4c15447a06cd9
shichida <m.shichida@trecon.co.jp>
2020-07-21 15:02:07 +0900

    テスト作成

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id             :bigint           not null, primary key
+#  name(チーム名) :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 FactoryBot.define do
   factory :team do
     name { '埼玉西武ライオンズ' }
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: teams
+#
+#  id             :bigint           not null, primary key
+#  name(チーム名) :string           not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe Team, type: :model do

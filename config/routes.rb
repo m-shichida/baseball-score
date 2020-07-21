8158ea0cb3a55d6df46eb918636f553abb303354
shichida <m.shichida@trecon.co.jp>
2020-07-21 17:34:32 +0900

    チーム一覧画面のビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :teams, only: %i[index, create]
+  resources :teams, only: %i[index create]
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
   },
   variants: {},
   plugins: [

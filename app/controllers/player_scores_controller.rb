b43a1da1454b0f2e1368a5c403e9ddba7f74996a
shichida <m.shichida@trecon.co.jp>
2020-07-27 23:34:05 +0900

    選手成績一覧コントローラ作成

---
+++ app/controllers/player_scores_controller.rb
@@ -0,0 +1,4 @@
+class PlayerScoresController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'games#index'
 
-  resources :players, only: %i[index create update destroy]
-  resources :teams,   only: %i[index create update destroy]
-  resources :games,   only: %i[index new create edit update destroy]
+  resources :players,       only: %i[index create update destroy]
+  resources :teams,         only: %i[index create update destroy]
+  resources :games,         only: %i[index new create edit update destroy]
+  resources :player_scores, only: %i[index]
 end

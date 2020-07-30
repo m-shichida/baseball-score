63331699fba4f7a52aaddf093f388aa7b9f95634
shichida <m.shichida@trecon.co.jp>
2020-07-30 21:58:28 +0900

    打者・投手成績のthをI18n化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def t_ar(attribute)
+    model_name = attribute.split('.')[0].underscore.delete("\b")
+    attribute = attribute.split('.')[1]
+
+    attribute.nil? ? t("activerecord.models.#{model_name}") : t("activerecord.attributes.#{model_name}.#{attribute}")
+  end
 end
